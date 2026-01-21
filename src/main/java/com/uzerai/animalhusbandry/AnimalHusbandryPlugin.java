dca0b0502b6317d172221d1a6c3f632ac3efbf79
E. Botten <edvbot@gmail.com>
2026-01-21 17:10:27 +0100

    chore(comments): remove goal comments which have been achieved

--- src/main/java/com/uzerai/animalhusbandry/AnimalHusbandryPlugin.java
+++ src/main/java/com/uzerai/animalhusbandry/AnimalHusbandryPlugin.java
@@ -55,14 +55,6 @@ public class AnimalHusbandryPlugin extends JavaPlugin {
         getEntityStoreRegistry().registerSystem(new GrowthRegisterSystem(NPCEntity.getComponentType(), getGrowthComponentType()));
         getEntityStoreRegistry().registerSystem(new GrowthSystem(NPCEntity.getComponentType(), getGrowthComponentType()));
 
-        // Need to listen to spawn events, and add a GrowthComponent from the GrowthComponentBuilder with the correct type
-        // As a first-time setup,  maybe also just add the entity to any NPCEntity with a role that has a defined GrowthConfig.
-        // Need to listen to ticks, and when the delta time between ticks is > config.GrowthTickInterval, increment all GrowthComponent currentGrowth instances
-        // whose lastGrowthInstant is also longer than config.GrowthTickInterval ago.
-        // Then need to send out events for "growing up" the ones which have tipped past their Role's "GrowthMax"
-        // And then replace them with ONE of the "TargetRole"s in the "TargetRole" array.
-
-        // Register growth section of plugin.
     }
 
     @Override
