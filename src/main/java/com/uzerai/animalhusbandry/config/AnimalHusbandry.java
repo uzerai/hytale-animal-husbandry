150a340afa7455ae6b4d92958e7b5e6f089ae663
E. Botten <edvbot@gmail.com>
2026-01-28 08:30:41 +0100

    chore(ah-config): remove unused config value, leave placeholder class in case of extension

--- src/main/java/com/uzerai/animalhusbandry/config/AnimalHusbandry.java
+++ src/main/java/com/uzerai/animalhusbandry/config/AnimalHusbandry.java
@@ -9,26 +9,15 @@ import javax.annotation.Nonnull;
 public class AnimalHusbandry {
     public static final BuilderCodec<AnimalHusbandry> CODEC =
             BuilderCodec.builder(AnimalHusbandry.class, AnimalHusbandry::new)
-                    .append(new KeyedCodec<>("BreedingEnabled", Codec.BOOLEAN),
-                            (cfg, v) -> cfg.breedingEnabled = v,
-                            cfg -> cfg.breedingEnabled)
-                    .add()
                     .build();
 
-    public boolean breedingEnabled = true;
 
     public AnimalHusbandry() {}
 
-    public boolean isBreedingEnabled(){
-        return breedingEnabled;
-    }
 
     @Nonnull
     @Override
     public String toString() {
-        return String.format(
-                "AnimalHusbandryConfig { breedingEnabled: %s }",
-                breedingEnabled
-        );
+        return String.format("AnimalHusbandryConfig {}");
     }
 }
